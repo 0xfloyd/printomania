3c73ba386402536c8954f2430c62fcafc582cac9
Muhammad Ahmad <73386311+0xf10yd@users.noreply.github.com>
2021-08-09 15:11:33 +0200

    Update DBConn.java

--- src/com/controller/DBConn.java
+++ src/com/controller/DBConn.java
@@ -5,8 +5,8 @@ import java.sql.*;
 public class DBConn {
 	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
 	static final String DB_URL = "jdbc:mysql://ec2-54-197-39-122.compute-1.amazonaws.com:3306/print_o_mania";
-	static final String USERNAME = "root";
-	static final String PASSWORD = "root";
+	static final String USERNAME = "";
+	static final String PASSWORD = "";
 	static Connection conn = null;
 	public static Connection getConnection(){
 		if(conn!=null){
